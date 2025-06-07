e6815fadd60728e4ba61f086860ff89f96ab2669
apokryptein <6676998+apokryptein@users.noreply.github.com>
2025-06-06 22:44:55 -0400

    feat: basic scaffolding
    Added basic scaffolding for the rxxd hex dump style program. The CLI
    takes one argument, the filename to read, reads it into a contents
    variable and prints the file's contents to stdout.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    // Filename
+    #[arg(short, long)]
+    filename: String,
+}
+
+fn main() -> Result<()> {
+    // Parse args
+    let args = Args::parse();
+
+    // Open file
+    println!("Opening: {}", args.filename);
+    let mut file = File::open(args.filename)?;
+
+    // Read file into contents
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    // Output results
+    println!("{}", contents);
+
+    Ok(())
 }
