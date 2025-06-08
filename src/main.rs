65ccdaba35d0119748ecf856ffa86db593f0da53
apokryptein <6676998+apokryptein@users.noreply.github.com>
2025-06-08 14:10:38 -0400

    feat: drew the owl it seems
    Implemented hex printing from a given file while accounting for various
    formatting features such as number of columns per line and number of
    bytes in each group per line. This behavior mimics xxd, and these
    behaviors are decided via cli flags.
    
    Additionally, I added the ability for the user to specify a number of
    bytes to be read as well as a seek or offset value from which to start
    reading in the provided input. These are also able to be configured via
    cli flags at runtime.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,53 @@ use std::fs::File;
 use std::io::{Read, Seek, SeekFrom};
 
 #[derive(Parser, Debug)]
-#[command(author, version, about)]
+#[command(
+    author = "apokryptein",
+    version,
+    about = "xxd-like hex dump utility written in Rust"
+)]
 struct Args {
     // Filename
     #[arg(short, long)]
     filename: String,
+
+    // Columns
+    #[arg(
+        short,
+        long,
+        default_value_t = 16,
+        help = "Number of octets (bytes) per line"
+    )]
+    cols: usize,
+
+    // Group size
+    // Default: 2 -> follows xxd standard
+    #[arg(
+        short,
+        long,
+        default_value_t = 2,
+        help = "Byte groupsize within each line"
+    )]
+    groupsize: usize,
+
+    // Length -> stop after number of bytes
+    #[arg(
+        short,
+        long,
+        default_value_t = 0,
+        help = "Number of bytes to read. Defaults to entire input."
+    )]
+    len: usize,
+
+    // Seek -> starts at offset
+    // Default: 0 -> start of input
+    #[arg(
+        short,
+        long,
+        default_value_t = 0,
+        help = "Start at <seek> bytes abs infile offset."
+    )]
+    seek: u64,
 }
 
 fn main() -> Result<()> {
@@ -24,19 +66,78 @@ fn main() -> Result<()> {
         }
     };
 
-    // Get file length
+    // Get file size
     let meta = file.metadata()?;
-    let file_len = meta.len() as usize;
+    let file_size = meta.len() as usize;
 
-    // Read file
-    // Leaving start at 0 for now and end at end of file
-    // Will change this to values specified from CLI
-    file.seek(SeekFrom::Start(0))?;
-    let mut buffer = vec![0; file_len];
+    // Set read length based on args.len and args.seek
+    let read_len: usize = match args.len {
+        // If no length arg is provided
+        0 => {
+            // If no seek then starting at 0
+            if args.seek == 0 {
+                // Read length will be size of input (file)
+                file_size
+            // If args.seek provided
+            } else {
+                // Read length is size of file minus args.seek
+                file_size - (args.seek as usize)
+            }
+        }
+        // If not 0, then just use provided size
+        _ => args.len,
+    };
+
+    // Ensure requested read size does not exceed the size of the provided data
+    if read_len > file_size {
+        bail!("[!] Cannot specify a read size larger than the file");
+    }
+
+    // Read file from args.seek offset (default of 0)
+    file.seek(SeekFrom::Start(args.seek))?;
+    let mut buffer = vec![0; read_len];
     file.read_exact(&mut buffer)?;
 
-    // Output results
-    println!("{:?}", buffer);
+    // Track offset address
+    let mut address: usize = 0;
+
+    // Construct and print hex according to desired format via
+    // cols and groupsize
+    for chunk in buffer.chunks(args.cols) {
+        let hex_string = chunk
+            .chunks(args.groupsize)
+            .map(|byte_size| {
+                byte_size
+                    .iter()
+                    .map(|b| format!("{:02x}", b))
+                    .collect::<Vec<String>>()
+                    .join("")
+            })
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        // Print line
+        println!("{:08x}:  {}  {}", address, hex_string, get_string(chunk),);
+
+        // Update address
+        address += args.cols;
+    }
 
     Ok(())
+}
+
+// get_string converts a byte array to a printable ASCII string
+fn get_string(byte_array: &[u8]) -> String {
+    let build_string_vec: Vec<String> = byte_array
+        .iter()
+        .map(|num| {
+            if *num > 32 && *num <= 126 {
+                (*num as char).to_string()
+            } else {
+                '.'.to_string()
+            }
+        })
+        .collect();
+
+    build_string_vec.join("")
 }
